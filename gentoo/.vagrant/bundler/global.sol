525a62df4b2ae4aba77cc994c5b7a6739f21df5c
sudden <sudden_che@icloud.com>
2022-06-24 16:51:59 +0300

    gentoo

---
+++ gentoo/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0.6.4"]],["trollop",["~> 2.1"]],["mini_portile2",["~> 2.4.0"]],["nokogiri",["= 1.10.10"]],["json",[">= 1.8"]],["builder",["~> 3.0"]],["rbvmomi",[">= 1.11.5","< 2.0.0"]],["vagrant-vsphere",["= 1.13.5"]]],"checksum":"d97b4950d4c78ceecca1ce45dff21e7eee1987b2a394677b16ecbf9548289cdd","vagrant_version":"2.2.19"}
